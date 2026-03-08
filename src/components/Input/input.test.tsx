1e27ac44e4b073c12c204bfd832cd6d748afeedc
lubnniamorais <lubnnia@gmail.com>
2026-03-08 15:57:51 -0300

    chore: Add function debug in input test

--- src/components/Input/input.test.tsx
+++ src/components/Input/input.test.tsx
@@ -3,5 +3,9 @@ import { render } from '@testing-library/react-native';
 import { Input } from '.';
 
 describe('Component: Input', () => {
-  it('should be ', () => {});
+  it('should be render without activity indicator', () => {
+    // A função render é para renderizar o componente
+    const { debug } = render(<Input />);
+    debug();
+  });
 });
