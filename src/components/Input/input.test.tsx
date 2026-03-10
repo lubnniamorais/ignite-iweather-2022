19da84070a8b9d8dea43958983ab85ce2f6d1db0
lubnniamorais <lubnnia@gmail.com>
2026-03-10 09:29:02 -0300

    chore: Include test for show toBeTruthy function

--- src/components/Input/input.test.tsx
+++ src/components/Input/input.test.tsx
@@ -10,4 +10,11 @@ describe('Component: Input', () => {
     const activityIndicator = screen.queryByTestId('activity-indicator');
     expect(activityIndicator).toBeNull();
   });
+
+  it('should be render without activity indicator if isLoading prop is true.', () => {
+    render(<Input isLoading />);
+
+    const activityIndicator = screen.getByTestId('activity-indicator');
+    expect(activityIndicator).toBeTruthy();
+  });
 });
