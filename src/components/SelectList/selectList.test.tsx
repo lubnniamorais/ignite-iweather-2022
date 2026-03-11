2555adc009b57448416cfba0278a48ade7c04724
lubnniamorais <lubnnia@gmail.com>
2026-03-11 10:01:29 -0300

    feat: Create selectList test

---
+++ src/components/SelectList/selectList.test.tsx
@@ -0,0 +1,17 @@
+import { render, screen } from '@testing-library/react-native';
+
+import { SelectList } from '.';
+
+describe('Component: SelectList', () => {
+  it('should be return city details selected', () => {
+    const data = [
+      { id: '1', name: 'Campinas', latitude: 123, longitude: 456 },
+      { id: '2', name: 'Campo Grande', latitude: 789, longitude: 987 },
+    ];
+
+    render(<SelectList data={data} onChange={() => {}} onPress={() => {}} />);
+
+    const selectedCity = screen.getByText(/campo/i);
+    console.log(selectedCity);
+  });
+});
