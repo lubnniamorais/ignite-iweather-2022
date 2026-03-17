a5e80761fa3f2a51a3bc2c168551f44ee479505c
lubnniamorais <lubnnia@gmail.com>
2026-03-17 10:45:59 -0300

    feat: Create tests/mocks folder

--- src/services/getCityByNameService.test.ts
+++ src/services/getCityByNameService.test.ts
@@ -1,22 +1,11 @@
+import { mockCityAPIResponse } from '@__tests__/mocks/mockCityAPIResponse';
 import { api } from './api';
 import { getCityByNameService } from './getCityByNameService';
 
 describe('API: getCityByNameService', () => {
   it('should return city details', async () => {
-    const data = {
-      id: '1',
-      city: 'São Paulo',
-      sys: {
-        country: 'BR',
-      },
-      coord: {
-        lat: 123,
-        lon: 456,
-      },
-    };
-
     jest.spyOn(api, 'get').mockResolvedValue({
-      data,
+      data: mockCityAPIResponse,
     });
 
     const response = await getCityByNameService('São Paulo');
