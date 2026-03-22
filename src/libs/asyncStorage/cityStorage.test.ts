7558cf04341763dce8293841db6f2323c1c7ba20
lubnniamorais <lubnnia@gmail.com>
2026-03-22 16:33:57 -0300

    feat: Add mock to sync storage test

---
+++ src/libs/asyncStorage/cityStorage.test.ts
@@ -0,0 +1,37 @@
+import { CityProps } from '@services/getCityByNameService';
+import {
+  getStorageCity,
+  removeStorageCity,
+  saveStorageCity,
+} from './cityStorage';
+
+const newCity: CityProps = {
+  id: '1',
+  name: 'São Paulo',
+  latitude: 123,
+  longitude: 456,
+};
+
+describe('Storage: CityStorage', () => {
+  it("should be return null when don't have a city storaged", async () => {
+    const response = await getStorageCity();
+
+    expect(response).toBeNull();
+  });
+
+  it('should be return city storaged', async () => {
+    await saveStorageCity(newCity);
+
+    const response = await getStorageCity();
+
+    expect(response).toEqual(newCity);
+  });
+
+  it('should be remove city storage', async () => {
+    await saveStorageCity(newCity);
+    await removeStorageCity();
+
+    const response = await getStorageCity();
+    expect(response).toBeNull();
+  });
+});
