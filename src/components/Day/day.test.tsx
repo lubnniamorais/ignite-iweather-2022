189d63c48859302ea5b4b6bc33e61597332a7ffe
lubnniamorais <lubnnia@gmail.com>
2026-03-23 16:27:05 -0300

    feat: Create test to day component

---
+++ src/components/Day/day.test.tsx
@@ -0,0 +1,20 @@
+import clearDay from '@assets/clear_day.svg';
+
+import { Day } from '@components/Day';
+import { render, screen } from '@testing-library/react-native';
+
+describe('Component: Day', () => {
+  it('should be render day', () => {
+    render(
+      <Day
+        data={{
+          day: '23/03',
+          min: '21°C',
+          max: '30°C',
+          icon: clearDay,
+          weather: 'Nublado',
+        }}
+      />
+    );
+  });
+});
