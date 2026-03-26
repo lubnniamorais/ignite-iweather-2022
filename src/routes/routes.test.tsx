8a97544d180bea4efa315ebd5d21c4ef2eb603d8
lubnniamorais <lubnnia@gmail.com>
2026-03-26 15:44:09 -0300

    feat: Create mock to react-native-safe-area-context

---
+++ src/routes/routes.test.tsx
@@ -0,0 +1,12 @@
+import { render, screen, waitFor } from '@testing-library/react-native';
+import { Routes } from '.';
+
+describe('Routes', () => {
+  it('should be render Search screen when not city selected', async () => {
+    render(<Routes />);
+
+    const title = await waitFor(() => screen.findByText(/^escolha um local/i));
+
+    expect(title).toBeTruthy();
+  });
+});
