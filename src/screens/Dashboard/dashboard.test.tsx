3cb9af000bd76c726bb88cedf22a9acfdc727b8f
lubnniamorais <lubnnia@gmail.com>
2026-03-31 11:11:12 -0300

    feat: Create integration test Dashboard screen

---
+++ src/screens/Dashboard/dashboard.test.tsx
@@ -0,0 +1,58 @@
+import { mockCityAPIResponse } from '@__tests__/mocks/api/mockCityAPIResponse';
+import { mockWeatherAPIResponse } from '@__tests__/mocks/api/mockWeatherAPIResponse';
+import { render, screen, waitFor } from '@__tests__/utils/customRender';
+import { saveStorageCity } from '@libs/asyncStorage/cityStorage';
+
+import { Dashboard } from '@screens/Dashboard';
+import { api } from '@services/api';
+
+describe('Screen: Dashboard', () => {
+  it('should be show a city weather', async () => {
+    jest.spyOn(api, 'get').mockResolvedValue({
+      data: mockWeatherAPIResponse,
+    });
+
+    const city = {
+      id: '1',
+      name: 'São Paulo, BR',
+      latitude: 123,
+      longitude: 456,
+    };
+
+    await saveStorageCity(city);
+
+    await waitFor(() => render(<Dashboard />));
+
+    const cityName = await screen.findByText(/são paulo/i);
+
+    expect(cityName).toBeTruthy();
+  });
+
+  it('should be show another selected weather city', async () => {
+    const city = {
+      id: '1',
+      name: 'São Paulo, BR',
+      latitude: 123,
+      longitude: 456,
+    };
+
+    await saveStorageCity(city);
+
+    /**
+     * 1 - Busca as informações do tempo/clima da cidade selecionada.
+     * 2 - Busca as informações da cidade.
+     * 3 - Busca as informações do tempo/clima da nova cidade selecionada.
+     */
+    jest
+      .spyOn(api, 'get')
+      .mockResolvedValueOnce({
+        data: mockWeatherAPIResponse,
+      })
+      .mockResolvedValueOnce({
+        data: mockCityAPIResponse,
+      })
+      .mockResolvedValueOnce({
+        data: mockWeatherAPIResponse,
+      });
+  });
+});
